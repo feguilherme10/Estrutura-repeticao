c646843c509be93148353e0bf758beba6cd07a81
feguilherme <jardimferreiraguilherme@gmail.com>
2026-02-06 19:40:29 -0300

    tabuada

--- contador.py
+++ contador.py
@@ -1,1 +1,25 @@
+variavel = True # boolean
+
+contador = 0
+
+while contador <=10:
+    print(contador)
+    contador = contador + 1
+
+
+    '''
+    qual a logica de uma tabuada? quero fazer a tabuada do 5
+    e deve mostrar:
+
+    5 x 1 = 5
+    5 x 2 = 10
+    5 x 3 = 15
+
+    ''' 
+    tabuada = 5
+ 
+
+    while contador <=10: 
+        print(tabuada, "x", contador, "=", contador * tabuada)
+        contador +=1
 
